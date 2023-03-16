f4c7660509cb0870ee7f5ea54b8f0d77518382f9
Oroko-bilhah <orokobilhah@gmail.com>
2023-03-16 11:20:53 -0700

    5-printc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - my 5th taslk
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
